52bb86e345507da956a3253aef29ae625e1a9292
Bon Champion <bonchampion@gmail.com>
2025-04-18 15:18:36 -0400

    Remove commented-out requirements from ContentView

--- Names and Faces/Names and Faces/ContentView.swift
+++ Names and Faces/Names and Faces/ContentView.swift
@@ -55,10 +55,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-//
-//    - Use PhotosPicker to let users import a photo from their photo library.
-//    - Detect when a new photo is imported, and immediately ask the user to name the photo.
-//    - Save that name and photo somewhere safe.
-//    - Show all names and photos in a list, sorted by name.
-//    - Create a detail screen that shows a picture full size.
-//    - Decide on a way to save all this data.
