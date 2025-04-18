7ef260882f648ad0080e46cc19e9ca1043a7ee59
Bon Champion <bonchampion@gmail.com>
2025-04-18 15:22:06 -0400

    Add initial Hot Prospects project

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Bon Champion on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
